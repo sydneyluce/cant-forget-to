b0721894748800caa7ce8a5aaca63bce08b74319
Sydney Luce <sydneytluce@gmail.com>
2020-09-12 15:39:44 -0400

    Built out some basic demo functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'todo-list';
+export class AppComponent implements OnInit {
+	public showNewTodoForm = false;
+	public todoForm: FormGroup;
+
+	public todoList = [
+		"Walk dog",
+		"Walk cat",
+		"Walk snake"
+	];
+
+	public completedTodoList = [];
+
+	public ngOnInit(): void {
+		this.todoForm = new FormGroup({
+			description: new FormControl(null, Validators.required)
+		});
+	}
+
+
+	public addTodo(): void {
+		this.todoList.push( this.todoForm.value.description);
+
+		this._resetTodoForm();
+	}
+
+	public completeTodo(index: number): void {
+		const completedTodo = this.todoList[index];
+
+		this.completedTodoList.push(completedTodo);
+
+		this.removeTodo(index);
+	}
+
+	public removeTodo(index: number): void {
+		this.todoList.splice(index, 1);
+	}
+
+	public toggleTodoForm(): void {
+		this._resetTodoForm();
+
+		this.showNewTodoForm = !this.showNewTodoForm;
+	}
+
+	private _resetTodoForm(): void {
+		this.todoForm.reset();
+		this.todoForm.markAsPristine();
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
@@ -13,6 +20,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
 	BrowserAnimationsModule,
+	ReactiveFormsModule,
+	MatButtonModule,
+	MatCardModule,
+	MatIconModule,
+	MatInputModule,
+	MatFormFieldModule,
+	MatListModule,
 	MatToolbarModule
   ],
   providers: [],
